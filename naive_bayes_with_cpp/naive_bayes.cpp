8378db5c73ffaa46cefc8c789c656ef3703cc6f7
pedroDubiela95 <pedrodubielabio@gmail.com>
2024-05-24 18:54:51 -0300

    feat: add naive_bayes.cpp last version

--- naive_bayes_with_cpp/naive_bayes.cpp
+++ naive_bayes_with_cpp/naive_bayes.cpp
@@ -2,10 +2,8 @@
 #pragma GCC diagnostic push												
 #pragma GCC diagnostic ignored "-Wsign-compare"
 
-
 // Libs
 #include <iostream>  // Entrada/Saída de dados
-#include <chrono>    // Medir tempo de execução do script
 #include <fstream>   // Manipulação de arquivos
 #include <vector>    // Vetores
 #include <string>    // trabalhar com strings
@@ -16,11 +14,10 @@
 
 // Namespace
 using namespace std;
-using namespace std::chrono;
 
+//----------------------------------------------Constanstes e Estruturas---------------------------------------------------
 // Ìndice de início dos dados de teste
 const int starTest = 900;
-
 
 // Struc
 struct DataFrame {
@@ -33,32 +30,67 @@ struct DataFrame {
         data(nrow, vector<double> (ncols, 0)) {}
 };
 
-
-// Funções
+//----------------------------------------------Funções--------------------------------------------------------------------
+// Leitura do arquivo .csv
 ifstream read_file(string file_path);
+
+// Instancia um objeto do tipo DataFrame
 DataFrame create_DataFrame(vector<string> cols, vector<vector<double>> data);
+
+// Printa um dataframe
 void print_DataFrame(DataFrame df, int n, string where = "top");
+
+// Reliza o slice em vetorores 1D
 vector<double> slice(vector<double> v, int start, int end);
+
+// Calcula uma tabela de frequências para a feature certificado_valido
 vector<vector<double>> get_distribution_certificado_valido(DataFrame df, vector<vector<double>> tab);
+
+// Calcula uma tabela de frequências para a feature tipo_doc
 vector<vector<double>> get_distribution_tipo_doc(DataFrame df, vector<vector<double>> tab);
+
+// Calcula a média para a váriável target classe quando y = 0 e quando y = 1
 vector<double>  calc_mean(DataFrame df);
+
+// Calcula o desvio padrão para a váriável target classe quando y = 0 e quando y = 1
 vector<double> calc_std(DataFrame df, vector<double> mean);
+
+// Encapsula a execução das funções 
+// --> get_distribution_certificado_valido
+// --> get_distribution_tipo_doc
+// --> calc_mean
+// --> calc_std
+// Gerando um vetor de vetores do tipo double que representa o modelo treinado
 vector<vector<vector<double>>> fit(DataFrame df);
+
+// Calcula as verossimilhanças P(certificado_valido = x | classe = y)
 double likelihood_certificado_valido(vector<vector<vector<double>>> probs, int y, double val_x);
+
+// Calcula as verossimilhanças P(tipo_doc = x | classe = y)
 double likelihood_tipo_doc(vector<vector<vector<double>>> probs, int y, double val_x);
+
+// Calcula as verossimilhanças P(uso_dias = x | classe = y)
 double likelihood_uso_dias(vector<vector<vector<double>>> probs, int y, double val_x);
+
+// Calcula as probabilidades à priori P(classe = 0) e P(classe = 1) 
 vector<double> priori(DataFrame df);
+
+// Aplica o modelo na base de testes
 vector<double> apply_model(DataFrame df_test, DataFrame df_train, vector<vector<vector<double>>> probs);
+
+// Avalia a performance do modelo obtido
+void evaluate(vector<double> y_true, vector<double> y_pred);
+
 
 int main() {
 
-    //------------------- Leitura da base de dados-------
+    cout << endl <<"#################### Naive Bayes #########################" << endl << endl;
+
+    //----------------------------------------------Leitura da base de dados---------------------------------------------------
     ifstream inputFile;
     inputFile = read_file("src/dataset.csv");
 
-
-    //------------------- Declaração das Vars ---------------
-
+    //----------------------------------------------Declaração das Vars -------------------------------------------------------
     // Variáveis escalares do tipo double para tratar os valores de cada coluna
 	double idVal;
 	double tipo_docVal;
@@ -82,8 +114,7 @@ int main() {
     // Pegar a primeira linha do aqruivo csv
     getline(inputFile, header);
 
-    //------------------- Loop de Carga e Limpeza dos Dados ---------------
-
+    //---------------------------------------------Loop de Carga e Limpeza dos Dados-------------------------------------------
     // Loop de carga e limpeza inicial dos dados
     while(inputFile.good()) {
 				
@@ -97,7 +128,7 @@ int main() {
 		if(!cell.empty()) {								
 
 			idVal = stod(cell);   // str to double	
-			id.push_back(idVal); // Append do valor de x no vetor					
+			id.push_back(idVal);  // Append do valor de x no vetor					
 							
             // Leitura da coluna tipo_doc
 			getline(inputFile, cell, ',');
@@ -126,7 +157,7 @@ int main() {
 		}	
 	}
 
-    //------------------- Criação do DataFrame ---------------
+    //---------------------------------------------Criação dos DataFrames------------------------------------------------------
     vector<string> cols = {"id","certificado_valido", "classe", "tipo_doc", "uso_dias"};
     vector<vector<double>> data = {id, certificado_valido, classe, tipo_doc, uso_dias};
     DataFrame df(id.size(), cols.size());
@@ -136,11 +167,11 @@ int main() {
     cout << "#------------Dataframe - primeiras 5 linhas------------#"  << endl;
     print_DataFrame(df, 5);
     cout << endl;
-    cout << "#-------------Dataframe - últimas 5 linhas-------------#"  << endl;
-    print_DataFrame(df, 5, "bottom");
+    //cout << "#-------------Dataframe - últimas 5 linhas-------------#"  << endl;
+    //print_DataFrame(df, 5, "bottom");
     cout << endl <<"=======================================================" << endl;
 
-    //------------------- Dataset de Treino ----------------
+    //Treino
     vector<vector<double>> data_train;
     DataFrame df_train(starTest, cols.size());
     data_train = {
@@ -155,11 +186,11 @@ int main() {
     cout << "#---------Dataframe Treino - primeiras 5 linhas---------#" << endl;
     print_DataFrame(df_train, 5);
     cout << endl;
-    cout << "#----------Dataframe Treino - últimas 5 linhas----------#"<< endl;
-    print_DataFrame(df_train, 5, "bottom");
+    //cout << "#----------Dataframe Treino - últimas 5 linhas----------#"<< endl;
+    //print_DataFrame(df_train, 5, "bottom");
     cout << endl <<"=======================================================" << endl;
 
-    //------------------- Dataset de Teste ----------------
+    //Teste
     vector<vector<double>> data_test;
     DataFrame df_test(id.size() - starTest, cols.size());
     data_test = {
@@ -174,64 +205,27 @@ int main() {
     cout << "#---------Dataframe Teste - primeiras 5 linhas---------#" << endl;
     print_DataFrame(df_test, 5);
     cout << endl;
-    cout << "#---------Dataframe Teste - últimas 5 linhas----------#"  << endl;
-    print_DataFrame(df_test, 5, "bottom");
+    //cout << "#---------Dataframe Teste - últimas 5 linhas----------#"  << endl;
+    //print_DataFrame(df_test, 5, "bottom");
     cout << endl <<"=======================================================" << endl;
 
-    //------------------- Fit Model ----------------
+    //-----------------------------------------------Treina Modelo-------------------------------------------------------------
     vector<vector<vector<double>>> probs;
     probs = fit(df_train);
 
-    //------------------- Apply Model ----------------
+    //-----------------------------------------------Aplica Modelo-------------------------------------------------------------
     vector<double> y_pred = apply_model(df_test, df_train, probs);
 
-    //------------------- Evaluate ----------------
+    //-----------------------------------------------Avaliação-----------------------------------------------------------------
     vector<double> y_true;
-    for(auto row: df_test.data) {
-        y_true.push_back(row.at(2));
-    }
+    for(auto row: df_test.data) {y_true.push_back(row.at(2));};
+    evaluate(y_true, y_pred);
 
-    double tp = 0, tn = 0, fp = 0, fn = 0;
-    /*
-       1   0
-    1  tp  fn 
-    0  fp  tn
-   
-    */
-
-    for (int i = 0; i < y_pred.size(); i++) {
-
-        if(y_true.at(i) == 1 && y_pred.at(i) == 1){
-            tp++;
-        }
-        if(y_true.at(i) == 1 && y_pred.at(i) == 0){
-            fn++;
-        }
-
-        if(y_true.at(i) == 0 && y_pred.at(i) == 1){
-            fp++;
-        }
-
-        if(y_true.at(i) == 0 && y_pred.at(i) == 0){
-            tn++;
-        }
-    }
-
-    double acc  = (tp + tn)/(tp + tn + fp + fn);
-    double sens = tp/(tp + fn);
-    double spec = tn/(tn + fp);
-
-    cout << "Acurácia: " << acc  << endl 
-    << "Sensibilidade: " << sens << endl 
-    <<"Especificidade: " << spec << endl; 
-
-
+    cout << endl <<"##########################################################" << endl;
 };
 
+
 ifstream read_file(string file_path) {
-    /*
-    Leitura do arquivos de dados
-    */
 
     ifstream inputFile;
     inputFile.open(file_path);
@@ -243,6 +237,7 @@ ifstream read_file(string file_path) {
 
     return inputFile;
 };
+
 
 DataFrame create_DataFrame(vector<string> cols, vector<vector<double>> data) {
 
@@ -264,6 +259,7 @@ DataFrame create_DataFrame(vector<string> cols, vector<vector<double>> data) {
 
     return df;
 };
+
 
 void print_DataFrame(DataFrame df, int n, string where) {
 
@@ -304,6 +300,7 @@ void print_DataFrame(DataFrame df, int n, string where) {
 
 };
 
+
 vector<double> slice(vector<double> v, int start, int end) {
     // Verifica se os índices são válidos
     if (start < 0 || end > v.size() || start > end) {
@@ -311,6 +308,7 @@ vector<double> slice(vector<double> v, int start, int end) {
     }
     return vector<double>(v.begin() + start, v.begin() + end);
 }
+
 
 vector<vector<double>> get_distribution_certificado_valido(DataFrame df, vector<vector<double>> tab) {
 
@@ -345,11 +343,12 @@ vector<vector<double>> get_distribution_certificado_valido(DataFrame df, vector<
     cout <<"y = 0 "<<tab.at(0).at(0) << " ";
     cout << tab.at(0).at(1) << endl;
     cout <<"y = 1 "<<tab.at(1).at(0) << " ";
-    cout << tab.at(1).at(1);
+    cout << tab.at(1).at(1) << endl;
     cout << endl <<"=======================================================" << endl;
 
     return tab;
 }
+
 
 vector<vector<double>> get_distribution_tipo_doc(DataFrame df, vector<vector<double>> tab) {
 
@@ -400,11 +399,12 @@ vector<vector<double>> get_distribution_tipo_doc(DataFrame df, vector<vector<dou
     cout << tab.at(0).at(2) << endl;
     cout <<"y = 1 "<<tab.at(1).at(0) << " ";
     cout << tab.at(1).at(1) << " ";
-    cout << tab.at(1).at(2);
+    cout << tab.at(1).at(2) << endl;
     cout << endl <<"=======================================================" << endl;
 
     return tab;
 };
+
 
 vector<double> calc_mean(DataFrame df) {
 
@@ -434,11 +434,12 @@ vector<double> calc_mean(DataFrame df) {
     cout << "Médias da feature uso_dias" << endl;
     cout << "y = 0     y = 1" << endl;
     cout << mean_uso_dias.at(0) <<" ";
-    cout << mean_uso_dias.at(1);
+    cout << mean_uso_dias.at(1) << endl;
     cout << endl <<"=======================================================" << endl;
 
     return mean_uso_dias;
 };
+
 
 vector<double> calc_std(DataFrame df, vector<double> mean) {
 
@@ -468,17 +469,16 @@ vector<double> calc_std(DataFrame df, vector<double> mean) {
     cout << "Desvio padrão da feature uso_dias" << endl;
     cout << "y = 0     y = 1" << endl;
     cout << std_uso_dias.at(0) <<" ";
-    cout << std_uso_dias.at(1);
+    cout << std_uso_dias.at(1) << endl;
     cout << endl <<"=======================================================" << endl;
 
     return std_uso_dias;
 };
 
 
-
 vector<vector<vector<double>>> fit(DataFrame df) {
 
-    // ------------------------- Discrete Features ------------------------------------
+    // ------------------------- Discretas ------------------------------------
     vector<vector<double>>  probability_distribution_certificado_valido(2, vector<double>(2,0)); // 2 x 2
     vector<vector<double>>  probability_distribution_tipo_doc(2, vector<double>(3,0));           // 2 x 3
 
@@ -494,7 +494,7 @@ vector<vector<vector<double>>> fit(DataFrame df) {
         probability_distribution_tipo_doc
     );
 
-    // ------------------------- Continuous Features ------------------------------------
+    // ------------------------- Continuas ------------------------------------
     vector<double> mean_uso_dias(2,0);                                                 // 1x2 
     vector<double> std_uso_dias(2,0);                                                  // 1x2
     vector<vector<double>>  probability_distribution_uso_dias(2, vector<double>(2,0)); // 2 x 2
@@ -507,7 +507,7 @@ vector<vector<vector<double>>> fit(DataFrame df) {
         std_uso_dias
     };
 
-    // ------------------------- Final Result ------------------------------------
+    // ------------------------- Resultado ------------------------------------
         
     // Result
     vector<vector<vector<double>>> probs;
@@ -542,9 +542,10 @@ double likelihood_uso_dias(vector<vector<vector<double>>> probs, int y, double v
     num = exp(-pow((val_x- mean), 2)  /(2*var));
     pdf = num/den;
 
-    cout <<"y = "<<y<< "| valor =  " << val_x << "| mean = " << mean << "| std = " << std << "| pdf = " << pdf << endl;
+    //cout <<"y = "<<y<< "| valor =  " << val_x << "| mean = " << mean << "| std = " << std << "| pdf = " << pdf << endl;
     return pdf;
 }
+
 
 vector<double> priori(DataFrame df) {
 
@@ -571,9 +572,10 @@ vector<double> priori(DataFrame df) {
     return priori;
 };
 
+
 vector<double> apply_model(DataFrame df_test, DataFrame df_train, vector<vector<vector<double>>> probs) {
 
-    //posterior   
+    //posteriori   
     double posterior_0, posterior_1;
 
     //piori
@@ -593,7 +595,7 @@ vector<double> apply_model(DataFrame df_test, DataFrame df_train, vector<vector<
     // pred
     vector<double> y_pred;
 
-    // Calculo
+    // Calcula
     for (auto row: df_test.data) {
 
         // certificado_valido
@@ -617,7 +619,7 @@ vector<double> apply_model(DataFrame df_test, DataFrame df_train, vector<vector<
         posterior_0 = likelihood_0 * priori_0;
         posterior_1 = likelihood_1 * priori_1;
 
-        // normalize
+        // normaliza
         if(posterior_0 > posterior_1){
             y_pred.push_back(0);
         } else {
@@ -628,4 +630,34 @@ vector<double> apply_model(DataFrame df_test, DataFrame df_train, vector<vector<
 };
 
 
-    
+void evaluate(vector<double> y_true, vector<double> y_pred){
+
+    double tp = 0, tn = 0, fp = 0, fn = 0;
+
+    for (int i = 0; i < y_pred.size(); i++) {
+
+        if(y_true.at(i) == 1 && y_pred.at(i) == 1){
+            tp++;
+        }
+        if(y_true.at(i) == 1 && y_pred.at(i) == 0){
+            fn++;
+        }
+
+        if(y_true.at(i) == 0 && y_pred.at(i) == 1){
+            fp++;
+        }
+
+        if(y_true.at(i) == 0 && y_pred.at(i) == 0){
+            tn++;
+        }
+    }
+
+    double acc  = (tp + tn)/(tp + tn + fp + fn);
+    double sens = tp/(tp + fn);
+    double spec = tn/(tn + fp);
+
+    cout << "Avaliação do Modelo" << endl;
+    cout << "Acurácia: " << acc   << endl 
+    << "Sensibilidade: " << sens  << endl 
+    <<"Especificidade: " << spec  << endl; 
+}
